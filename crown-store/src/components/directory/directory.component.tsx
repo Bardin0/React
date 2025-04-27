e85e81d4465833cc71eb5f53e66abbb1dfc7a89b
Michael Giovannini <154377009+Bardin0@users.noreply.github.com>
2025-04-27 16:49:39 -0400

    Updated file structure and improved clarity of code

---
+++ crown-store/src/components/directory/directory.component.tsx
@@ -0,0 +1,19 @@
+import { Category } from "../../types/Category.types";
+import CategoryItem from "../category-item/category-item.component";
+import './directory.styles.scss'
+
+type Props = {
+    categories: Category[]
+}
+
+const Directory = ( { categories } : Props ) => {
+  return (
+    <div className="directory-container">
+      {categories.map((category) => (
+        <CategoryItem key={category.id} category={category} />
+      ))}
+    </div>
+  );
+};
+
+export default Directory;
