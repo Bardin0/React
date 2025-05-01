67a56ee5e6291468ac3b85588cc40f15cdfd9155
Michael Giovannini <154377009+Bardin0@users.noreply.github.com>
2025-05-01 13:00:59 -0400

    added some things

---
+++ crown-store/src/components/routes/navigation/navigation.component.tsx
@@ -0,0 +1,27 @@
+/// <reference types="vite-plugin-svgr/client" />
+import { Outlet, Link } from "react-router-dom";
+import { Fragment } from "react";
+import CrwnLogo from '../../../assets/crown.svg?react'
+
+import './navigation.styles.scss'
+
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <CrwnLogo className='logo'/>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="/shop">
+            SHOP
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
